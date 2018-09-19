eb3154c89fcdcfca529f42077396fd2271275931
scendejas427 <salcendejas427@gmail.com>
2018-09-19 15:44:40 -0400

    search for recipe and save to database

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
 import './App.css';
-
-import logo from './logo.svg';
-
+import GetRecipe from './components/getRecipe/GetRecipe.component';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <BrowserRouter>
+        <div>
+          <div id="main-content-container">
+          <Switch>
+            <Route path="/getRecipe" component={ GetRecipe } />
+            <Route component={ GetRecipe } />
+          </Switch>
+          </div>
       </div>
+      </BrowserRouter>
     );
   }
 }
